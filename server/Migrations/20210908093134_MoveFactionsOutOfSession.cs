1fd8842396812666ab08dd53cc60622193e32c68
tarnas14 <tarnas14@gmail.com>
2021-09-08 11:32:06 +0200

    created a migration for moving Factions out of session in db

---
+++ server/Migrations/20210908093134_MoveFactionsOutOfSession.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class MoveFactionsOutOfSession : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Factions",
+                table: "Sessions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "Factions",
+                table: "Sessions",
+                type: "text[]",
+                nullable: true);
+        }
+    }
+}
