f3261a5319190ad18faf96ad56a2443f1c679a12
tarnas14 <tarnas14@gmail.com>
2021-09-26 23:05:01 +0200

    showing success for user after saving details

--- client/src/SessionView/DetailsForm.js
+++ client/src/SessionView/DetailsForm.js
@@ -10,7 +10,9 @@ import {
   Input,
   InputLabel,
   Paper,
+  Snackbar,
 } from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert';
 
 import { ComboDispatchContext } from '../state'
 
@@ -25,6 +27,8 @@ const getNow = () => {
 function DetailsForm({
   session
 }) {
+  const [showSuccess, setShowSuccess] = useState(false)
+  const handleSnackbarClose = useCallback(() => setShowSuccess(false), [])
   const comboDispatch = useContext(ComboDispatchContext)
   const [sessionDisplayName, setSessionDisplayname] = useState(session.displayName || '')
   const [isTTS, setIsTTS] = useState(session.tts || false)
@@ -46,6 +50,7 @@ function DetailsForm({
       || (duration && duration !== session.duration)
 
     if (!changed) {
+      setShowSuccess(true)
       return
     }
 
@@ -59,81 +64,99 @@ function DetailsForm({
       duration,
     }
     comboDispatch({ type: 'MetadataUpdated', payload })
+    setShowSuccess(true)
   }, [session, sessionDisplayName, isTTS, isSplit, sessionStart, sessionEnd, duration, comboDispatch])
 
-  return <Paper><Container>
-    <form noValidate autoComplete="off">
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <FormControl fullWidth color='secondary'>
-            <InputLabel htmlFor="sessionName">Your session name</InputLabel>
-            <Input
-              id="sessionName"
-              value={sessionDisplayName}
-              onChange={getChangeHandler(setSessionDisplayname)}
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <FormGroup row>
-            <FormControlLabel
-              control={<Checkbox checked={isTTS} onChange={getChangeHandler(setIsTTS, 'checked')}/>}
-              label="TTS"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={isSplit} onChange={getChangeHandler(setIsSplit, 'checked')}/>}
-              label="Split"
-            />
-          </FormGroup>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth color='secondary'>
-            <InputLabel htmlFor="sessionStart">Session date</InputLabel>
-            <Input
-              id="sessionStart"
-              defaultValue={getNow()}
-              value={sessionStart || getNow()}
-              onChange={getChangeHandler(setSessionStart)}
-              type="date"
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          {isSplit && <FormControl fullWidth color='secondary'>
-            <InputLabel htmlFor="sessionEnd">Session end date</InputLabel>
-            <Input
-              id="sessionEnd"
-              defaultValue={getNow()}
-              value={sessionEnd || getNow()}
-              onChange={getChangeHandler(setSessionEnd)}
-              type="date"
-            />
-          </FormControl>}
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth color='secondary'>
-            <InputLabel htmlFor="sessionDuration">How long did you play? (roughly)</InputLabel>
-            <Input
-              id="sessionDuration"
-              value={duration}
-              inputProps={{ step: 0.25, min: 0}}
-              type="number"
-              onChange={getChangeHandler(setDuration)}
-              endAdornment='hours'
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} container justifyContent='flex-end'>
-          <Button
-            variant='contained'
-            color='secondary'
-            onClick={handleSave}
-          >Save</Button>
-        </Grid>
-      </Grid>
-    </form>
-  </Container>
-  </Paper>
+  return <>
+    <Paper>
+      <Container>
+        <form noValidate autoComplete="off">
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <FormControl fullWidth color='secondary'>
+                <InputLabel htmlFor="sessionName">Your session name</InputLabel>
+                <Input
+                  id="sessionName"
+                  value={sessionDisplayName}
+                  onChange={getChangeHandler(setSessionDisplayname)}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormGroup row>
+                <FormControlLabel
+                  control={<Checkbox checked={isTTS} onChange={getChangeHandler(setIsTTS, 'checked')}/>}
+                  label="TTS"
+                />
+                <FormControlLabel
+                  control={<Checkbox checked={isSplit} onChange={getChangeHandler(setIsSplit, 'checked')}/>}
+                  label="Split"
+                />
+              </FormGroup>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth color='secondary'>
+                <InputLabel htmlFor="sessionStart">Session date</InputLabel>
+                <Input
+                  id="sessionStart"
+                  defaultValue={getNow()}
+                  value={sessionStart || getNow()}
+                  onChange={getChangeHandler(setSessionStart)}
+                  type="date"
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              {isSplit && <FormControl fullWidth color='secondary'>
+                <InputLabel htmlFor="sessionEnd">Session end date</InputLabel>
+                <Input
+                  id="sessionEnd"
+                  defaultValue={getNow()}
+                  value={sessionEnd || getNow()}
+                  onChange={getChangeHandler(setSessionEnd)}
+                  type="date"
+                />
+              </FormControl>}
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth color='secondary'>
+                <InputLabel htmlFor="sessionDuration">How long did you play? (roughly)</InputLabel>
+                <Input
+                  id="sessionDuration"
+                  value={duration}
+                  inputProps={{ step: 0.25, min: 0}}
+                  type="number"
+                  onChange={getChangeHandler(setDuration)}
+                  endAdornment='hours'
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} container justifyContent='flex-end'>
+              <Button
+                variant='contained'
+                color='secondary'
+                onClick={handleSave}
+              >Save</Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
+    </Paper>
+    <Snackbar
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      open={showSuccess}
+      autoHideDuration={5000}
+      onClose={handleSnackbarClose}
+    >
+      <MuiAlert
+        elevation={6}
+        variant="filled"
+        severity="success"
+      >
+        Details saved correctly
+      </MuiAlert>
+    </Snackbar>
+  </>
 }
 
 export default DetailsForm
