bc8cecbbc2a32d5aa3aa92bb750fa52fc348b0a0
tarnas14 <tarnas14@gmail.com>
2021-12-06 23:55:22 +0100

    allowing user to add title and description to their timeline events

--- client/src/SessionView/Timeline/AddTimelineEvent.js
+++ client/src/SessionView/Timeline/AddTimelineEvent.js
@@ -1,42 +1,111 @@
 import { useCallback, useState } from 'react'
-import { Button } from '@material-ui/core'
+import { Button, TextField } from '@material-ui/core'
 import { Image as ImageIcon, Close as CloseIcon } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles'
 import { Trans } from 'react-i18next'
 
-import ImageUpload from '../../shared/ImageUpload'
+import ImagePicker from '../../shared/ImagePicker'
+
+const useStyles = makeStyles({
+  inputWithMargin: {
+    marginBottom: '1em',
+  },
+  formContainer: {
+    width: '300%',
+    maxWidth: '100vw',
+  },
+})
 
 function AddTimelineEvent({ uploadEvent }) {
+  const classes = useStyles()
   const [open, setOpen] = useState(false)
+  const [file, setFile] = useState(null)
 
-  const handleUpload = useCallback(
-    async (file) => {
-      await uploadEvent(file)
-      setOpen(false)
-    },
-    [uploadEvent],
-  )
+  const [title, setTitle] = useState('')
+  const handleTitle = useCallback((event) => {
+    const { value } = event.target
+
+    setTitle(value)
+  }, [])
+
+  const [description, setDescription] = useState('')
+  const handleDescription = useCallback((event) => {
+    const { value } = event.target
+
+    setDescription(value)
+  }, [])
+
+  const toggle = useCallback(() => {
+    if (open) {
+      setTitle('')
+      setDescription('')
+    }
+
+    setOpen(!open)
+  }, [open])
+
+  const handleUpload = useCallback(async () => {
+    await uploadEvent({ file, title, description })
+    toggle()
+  }, [file, description, title, uploadEvent, toggle])
+
+  const saveActive = file || description || title
 
   return (
     <>
       <Button
-        color="secondary"
-        onClick={() => setOpen((a) => !a)}
-        style={{ width: '15em' }}
+        color={open ? 'primary' : 'secondary'}
+        onClick={toggle}
+        style={{ width: open ? '4em' : '15em' }}
         variant="contained"
       >
         {open ? <CloseIcon /> : <Trans i18nKey="sessionTimeline.cta" />}
       </Button>
       {open && (
-        <ImageUpload
-          Icon={<ImageIcon style={{ fontSize: 40 }} />}
-          translations={{
-            changeFile: 'sessionTimeline.changeFile',
-            dropHere: 'sessionTimeline.dropHere',
-            dragHere: 'sessionTimeline.dragHere',
-            button: 'sessionTimeline.submit',
-          }}
-          upload={handleUpload}
-        />
+        <>
+          <Button
+            color="secondary"
+            disabled={!saveActive}
+            onClick={handleUpload}
+            style={{ width: '15em' }}
+            variant="contained"
+          >
+            Save
+          </Button>
+          <div className={classes.formContainer}>
+            <TextField
+              className={classes.inputWithMargin}
+              color="secondary"
+              fullWidth
+              label="What happened?!"
+              onChange={handleTitle}
+              value={title}
+              variant="filled"
+            />
+            <TextField
+              className={classes.inputWithMargin}
+              color="secondary"
+              fullWidth
+              label="What's the story"
+              multiline
+              onChange={handleDescription}
+              rows={3}
+              value={description}
+              variant="filled"
+            />
+            <ImagePicker
+              Icon={<ImageIcon style={{ fontSize: 40 }} />}
+              onChange={setFile}
+              previewAboveDropzone
+              translations={{
+                changeFile: 'sessionTimeline.changeFile',
+                dropHere: 'sessionTimeline.dropHere',
+                dragHere: 'sessionTimeline.dragHere',
+                button: 'sessionTimeline.submit',
+              }}
+            />
+          </div>
+        </>
       )}
     </>
   )
---
+++ client/src/shared/ImagePicker.js
@@ -0,0 +1,169 @@
+import { useState, useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { Grid, Snackbar } from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert'
+import { makeStyles } from '@material-ui/core/styles'
+import { useTranslation, Trans } from 'react-i18next'
+
+import useSmallViewport from './useSmallViewport'
+
+const useStyles = makeStyles({
+  dropzone: {
+    textAlign: 'center',
+    backgroundColor: 'rgba(255, 255, 255, .1)',
+    padding: '1em',
+    borderRadius: 5,
+    cursor: 'pointer',
+  },
+  previewContainer: {
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  previewWatermark: {
+    position: 'absolute',
+    textTransform: 'uppercase',
+    color: '#fff',
+    top: '50%',
+    width: '100%',
+    textAlign: 'center',
+    transform: 'translateY(-50%) rotate(45deg)',
+    transformOrigin: 'center center',
+    opacity: 0.2,
+    fontSize: ({ small }) => (small ? '3em' : '10em'),
+    margin: 0,
+    padding: 0,
+  },
+  mapPreview: {
+    maxWidth: '80%',
+    opacity: 0.85,
+  },
+})
+
+function ImagePicker({ onChange, translations, Icon, previewAboveDropzone }) {
+  const { t } = useTranslation()
+  const small = useSmallViewport()
+  const classes = useStyles({ small })
+  const [file, setFile] = useState(null)
+  const [previewUrl, setPreviewUrl] = useState('')
+  const [fileErrors, setFileErrors] = useState(null)
+
+  const onDropAccepted = useCallback(
+    (acceptedFiles) => {
+      setFileErrors(null)
+      const [theFile] = acceptedFiles
+
+      const url = URL.createObjectURL(theFile)
+      setPreviewUrl(url)
+      setFile(theFile)
+      onChange(theFile, url)
+    },
+    [onChange],
+  )
+
+  const onDropRejected = useCallback(
+    (fileRejections) => {
+      const [rejection] = fileRejections
+      setFile(null)
+      onChange(null)
+
+      setFileErrors(rejection.errors)
+    },
+    [onChange],
+  )
+
+  const clearFile = useCallback(() => {
+    setFile(null)
+    setFileErrors(null)
+    onChange(null)
+  }, [onChange])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDropAccepted,
+    onDropRejected,
+    maxFiles: 1,
+    maxSize: 3000000,
+    multiple: false,
+    accept: ['image/jpg', 'image/png', 'image/jpeg'],
+  })
+  const handleSnackbarClose = useCallback((_, reason) => {
+    if (reason === 'timeout') {
+      setFileErrors(null)
+    }
+  }, [])
+
+  return (
+    <>
+      {previewAboveDropzone && file && (
+        <Grid
+          className={classes.previewContainer}
+          container
+          item
+          justifyContent="center"
+        >
+          <img
+            alt={t('sessionMap.map')}
+            className={classes.mapPreview}
+            src={previewUrl}
+          />
+          <p className={classes.previewWatermark}>
+            <Trans i18nKey="general.labels.preview" />
+          </p>
+        </Grid>
+      )}
+      <Grid item>
+        <div {...getRootProps()} className={classes.dropzone}>
+          <input {...getInputProps()} />
+          {Icon}
+          {file && (
+            <p>
+              <Trans i18nKey={translations.changeFile} />
+            </p>
+          )}
+          {!file && isDragActive ? (
+            <p>
+              <Trans i18nKey={translations.dropHere} />
+            </p>
+          ) : (
+            <p>
+              <Trans i18nKey={translations.dragHere} />
+            </p>
+          )}
+          {translations.sizeHint && (
+            <p>
+              <Trans i18nKey={translations.sizeHint} />
+            </p>
+          )}
+        </div>
+      </Grid>
+      {!previewAboveDropzone && file && (
+        <Grid
+          className={classes.previewContainer}
+          container
+          item
+          justifyContent="center"
+        >
+          <img
+            alt={t('sessionMap.map')}
+            className={classes.mapPreview}
+            src={previewUrl}
+          />
+          <p className={classes.previewWatermark}>
+            <Trans i18nKey="general.labels.preview" />
+          </p>
+        </Grid>
+      )}
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        open={fileErrors}
+      >
+        <MuiAlert elevation={6} severity="error" variant="filled">
+          {(fileErrors || []).map((fe) => fe.message).join(';')}
+        </MuiAlert>
+      </Snackbar>
+    </>
+  )
+}
+
+export default ImagePicker
