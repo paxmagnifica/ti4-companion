0c79531fae140fb722deac9c4ba03ac459a8a0b2
tarnas14 <tarnas14@gmail.com>
2022-01-03 00:00:42 +0100

    added steppers for phases and player order

---
+++ client/src/SessionView/Overview/Drafting/PlayerOrderStepper.js
@@ -0,0 +1,62 @@
+import clsx from 'clsx'
+import { Typography, Stepper, Step, StepLabel } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  ArrowUpward as ActiveIcon,
+  AccessibilityNew as PlayerIcon,
+  Done as DoneIcon,
+} from '@material-ui/icons'
+
+const useStepperStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: 'transparent',
+  },
+  done: {
+    opacity: 0.7,
+  },
+}))
+
+export function PlayerOrderStepper({ order, activePlayer }) {
+  const classes = useStepperStyles()
+
+  return (
+    <>
+      <Typography align="center" variant="h4">
+        Player order:
+      </Typography>
+      <Stepper
+        activeStep={activePlayer}
+        alternativeLabel
+        className={classes.root}
+      >
+        {order.map((label, index) => (
+          <Step key={label} color="secondary">
+            <StepLabel
+              optional={
+                index === activePlayer ? (
+                  <Typography align="center">
+                    <ActiveIcon color="secondary" />
+                  </Typography>
+                ) : index < activePlayer ? (
+                  <Typography align="center">
+                    <DoneIcon color="secondary" />
+                  </Typography>
+                ) : null
+              }
+              StepIconComponent={PlayerIcon}
+              StepIconProps={{
+                className: clsx({ [classes.done]: index < activePlayer }),
+              }}
+            >
+              <Typography
+                className={clsx({ [classes.done]: index < activePlayer })}
+              >
+                {label}
+              </Typography>
+            </StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </>
+  )
+}
---
+++ client/src/SessionView/Overview/Drafting/index.js
@@ -0,0 +1,1 @@
+export { Drafting } from './Drafting'
