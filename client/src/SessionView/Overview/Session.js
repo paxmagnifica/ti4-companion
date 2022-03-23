c1a7053079f7df47e8c8b6728c85e848a75c3e78
tarnas14 <tarnas14@gmail.com>
2022-03-23 23:32:31 +0100

    fix linter errors

--- client/src/SessionView/Overview/Session.js
+++ client/src/SessionView/Overview/Session.js
@@ -1,10 +1,9 @@
-import { useEffect, useCallback } from 'react'
+import { useCallback } from 'react'
 import clsx from 'clsx'
 import { Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 import { HideInFullscreen, useFullscreen } from '../../Fullscreen'
-import { useSessionContext } from '../SessionProvider'
 
 import { SessionNutshell } from './SessionNutshell'
 import VictoryPoints from './VictoryPoints'
