3eda678e7cc28ef069f00cc57744b0be7590af40
tarnas14 <tarnas14@gmail.com>
2022-04-17 12:19:54 +0200

    showing public objectives points on vp helper

--- client/src/SessionView/Overview/VictoryPoints/PointsSourceHelper/PointsHistory.js
+++ client/src/SessionView/Overview/VictoryPoints/PointsSourceHelper/PointsHistory.js
@@ -1,5 +1,6 @@
 import { Fragment, useMemo } from 'react'
 import {
+  Grid,
   Button,
   ButtonGroup,
   List,
@@ -13,9 +14,10 @@ import { useObjectives } from '../../../../queries'
 import { useSessionContext } from '../../../useSessionContext'
 import FactionFlag from '../../../../shared/FactionFlag'
 import { ObjectiveSelector } from '../../../../shared/ObjectiveSelector'
+import Objective from '../../../../shared/Objective'
 import { VP_SOURCE } from '../../../../shared/constants'
 
-const pointsHistoryEvents = ['VictoryPointsUpdated']
+const pointsHistoryEvents = ['VictoryPointsUpdated', 'ObjectiveScored']
 
 export function PointsHistory({ factions }) {
   const {
@@ -24,9 +26,13 @@ export function PointsHistory({ factions }) {
   const { timeline } = useTimelineEvents({
     sessionId,
   })
-  const { objectives: availableObjectives } = useObjectives()
+  const { objectives } = useObjectives()
+  const availableObjectives = useMemo(
+    () => Object.values(objectives),
+    [objectives],
+  )
   const secretObjectives = useMemo(
-    () => Object.values(availableObjectives).filter((obj) => obj.secret),
+    () => availableObjectives.filter((obj) => obj.secret),
     [availableObjectives],
   )
 
@@ -34,9 +40,13 @@ export function PointsHistory({ factions }) {
     () =>
       timeline
         .filter(({ eventType }) => pointsHistoryEvents.includes(eventType))
-        .map(({ payload }) => ({
+        .map(({ payload, eventType }) => ({
+          context: payload.slug, // public objectives have slug instead of context
           ...payload,
-          source: VP_SOURCE.fromBackendToFrontend(payload.source),
+          source:
+            VP_SOURCE.fromBackendToFrontend(payload.source) ||
+            (eventType === 'ObjectiveScored' && VP_SOURCE.objective),
+          isPublic: eventType === 'ObjectiveScored',
         })),
     [timeline],
   )
@@ -44,121 +54,142 @@ export function PointsHistory({ factions }) {
 
   return (
     <List>
-      {pointsHistory.map(({ faction, points, source, context }, index) => (
-        <Fragment key={`${faction}->${points}`}>
-          <ListItem>
-            <ListItemIcon>
-              <FactionFlag
-                disabled
-                factionKey={faction}
-                height="2em"
-                selected
-                width="3em"
-              />
-            </ListItemIcon>
-            <ListItemText>
-              {'-> '}
-              {points}
-            </ListItemText>
-          </ListItem>
-          <ListItem>
-            <ButtonGroup>
-              <Button
-                color={source === VP_SOURCE.custodian ? 'secondary' : 'default'}
-                onClick={() =>
-                  addSource({
-                    index,
-                    faction,
-                    points,
-                    source: VP_SOURCE.custodian,
-                  })
-                }
-              >
-                Custodian
-              </Button>
-              <Button
-                color={source === VP_SOURCE.objective ? 'secondary' : 'default'}
-                onClick={() =>
-                  addSource({
-                    index,
-                    faction,
-                    points,
-                    source: VP_SOURCE.objective,
-                  })
-                }
-              >
-                Secret
-              </Button>
-              <Button
-                color={source === VP_SOURCE.mecatol ? 'secondary' : 'default'}
-                onClick={() =>
-                  addSource({
-                    index,
-                    faction,
-                    points,
-                    source: VP_SOURCE.mecatol,
-                  })
-                }
-              >
-                Mecatol
-              </Button>
-              <Button
-                color={source === VP_SOURCE.support ? 'secondary' : 'default'}
-                onClick={() =>
-                  addSource({
-                    index,
-                    faction,
-                    points,
-                    source: VP_SOURCE.support,
-                  })
-                }
-              >
-                SFT
-              </Button>
-            </ButtonGroup>
-          </ListItem>
-          {source === VP_SOURCE.objective && (
+      {pointsHistory.map(
+        ({ faction, points, source, context, isPublic }, index) => (
+          <Fragment key={`${faction}->${points}`}>
             <ListItem>
-              <ObjectiveSelector
-                objectives={secretObjectives}
-                onChange={(selectedObjective) =>
-                  addSource({
-                    index,
-                    faction,
-                    points,
-                    source: VP_SOURCE.objective,
-                    context: selectedObjective.slug,
-                  })
-                }
-                value={secretObjectives.find((o) => o.slug === context)}
-              />
-            </ListItem>
-          )}
-          {source === VP_SOURCE.support && (
-            <ListItem>
-              {factions.map((factionKey) => (
+              <ListItemIcon>
                 <FactionFlag
-                  key={factionKey}
-                  disabled={faction === factionKey}
-                  factionKey={factionKey}
+                  disabled
+                  factionKey={faction}
                   height="2em"
+                  selected
+                  width="3em"
+                />
+              </ListItemIcon>
+              <ListItemText>
+                {'-> '}
+                {points}
+              </ListItemText>
+            </ListItem>
+            <ListItem>
+              <ButtonGroup>
+                <Button
+                  color={
+                    source === VP_SOURCE.custodian ? 'secondary' : 'default'
+                  }
+                  disabled={isPublic}
+                  onClick={() =>
+                    addSource({
+                      index,
+                      faction,
+                      points,
+                      source: VP_SOURCE.custodian,
+                    })
+                  }
+                >
+                  Custodian
+                </Button>
+                <Button
+                  color={
+                    source === VP_SOURCE.objective ? 'secondary' : 'default'
+                  }
+                  onClick={
+                    isPublic
+                      ? () => null
+                      : () =>
+                          addSource({
+                            index,
+                            faction,
+                            points,
+                            source: VP_SOURCE.objective,
+                          })
+                  }
+                >
+                  Objective
+                </Button>
+                <Button
+                  color={source === VP_SOURCE.mecatol ? 'secondary' : 'default'}
+                  disabled={isPublic}
+                  onClick={() =>
+                    addSource({
+                      index,
+                      faction,
+                      points,
+                      source: VP_SOURCE.mecatol,
+                    })
+                  }
+                >
+                  Mecatol
+                </Button>
+                <Button
+                  color={source === VP_SOURCE.support ? 'secondary' : 'default'}
+                  disabled={isPublic}
                   onClick={() =>
                     addSource({
                       index,
                       faction,
                       points,
                       source: VP_SOURCE.support,
-                      context: factionKey,
                     })
                   }
-                  selected={factionKey === context}
-                  width="2.5em"
-                />
-              ))}
+                >
+                  SFT
+                </Button>
+              </ButtonGroup>
             </ListItem>
-          )}
-        </Fragment>
-      ))}
+            {source === VP_SOURCE.objective && !isPublic && (
+              <ListItem>
+                <ObjectiveSelector
+                  objectives={secretObjectives}
+                  onChange={(selectedObjective) =>
+                    addSource({
+                      index,
+                      faction,
+                      points,
+                      source: VP_SOURCE.objective,
+                      context: selectedObjective.slug,
+                    })
+                  }
+                  value={secretObjectives.find((o) => o.slug === context)}
+                />
+              </ListItem>
+            )}
+            {source === VP_SOURCE.objective && isPublic && (
+              <ListItem>
+                <Grid container justifyContent="center">
+                  <Objective
+                    {...availableObjectives.find((o) => o.slug === context)}
+                  />
+                </Grid>
+              </ListItem>
+            )}
+            {source === VP_SOURCE.support && (
+              <ListItem>
+                {factions.map((factionKey) => (
+                  <FactionFlag
+                    key={factionKey}
+                    disabled={faction === factionKey}
+                    factionKey={factionKey}
+                    height="2em"
+                    onClick={() =>
+                      addSource({
+                        index,
+                        faction,
+                        points,
+                        source: VP_SOURCE.support,
+                        context: factionKey,
+                      })
+                    }
+                    selected={factionKey === context}
+                    width="2.5em"
+                  />
+                ))}
+              </ListItem>
+            )}
+          </Fragment>
+        ),
+      )}
     </List>
   )
 }
