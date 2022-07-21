5eeb0ce5f72f000241c6ca4223eaff35c96cb16f
tarnas14 <tarnas14@gmail.com>
2022-07-22 01:07:25 +0200

    commented out unreachable code in tech queries

--- client/src/KnowledgeBase/TechnologyCards/queries.js
+++ client/src/KnowledgeBase/TechnologyCards/queries.js
@@ -6,34 +6,32 @@ import { useGameVersion } from '../../GameComponents'
 
 const queryKey = (gameVersion) => ['techs', gameVersion]
 
-export const useTechs = () => {
-  return {
-    techs: [],
-    queryInfo: {
-      isFetched: true,
-    },
-  }
+export const useTechs = () => ({
+  techs: [],
+  queryInfo: {
+    isFetched: true,
+  },
+})
 
-  const { gameVersion } = useGameVersion()
+// const { gameVersion } = useGameVersion()
 
-  const { data: techs, ...queryInfo } = useQuery(
-    queryKey(gameVersion),
-    async () => {
-      const result = await fetch(`${CONFIG.apiUrl}/api/techs`, {
-        headers: {
-          'x-ti4companion-game-version': gameVersion,
-        },
-      }).then(handleErrors)
+// const { data: techs, ...queryInfo } = useQuery(
+// queryKey(gameVersion),
+// async () => {
+// const result = await fetch(`${CONFIG.apiUrl}/api/techs`, {
+// headers: {
+// 'x-ti4companion-game-version': gameVersion,
+// },
+// }).then(handleErrors)
 
-      const results = await result.json()
+// const results = await result.json()
 
-      return results.reduce((accu, obj) => ({ ...accu, [obj.slug]: obj }), {})
-    },
-    { placeholderData: {} },
-  )
+// return results.reduce((accu, obj) => ({ ...accu, [obj.slug]: obj }), {})
+// },
+// { placeholderData: {} },
+// )
 
-  return {
-    techs,
-    queryInfo,
-  }
-}
+// return {
+// techs,
+// queryInfo,
+// }
