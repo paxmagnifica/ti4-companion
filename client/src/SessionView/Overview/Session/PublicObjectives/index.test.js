1fc9e738a18f6ce2afb10d9b50358ee2070c5364
Karola <karolina.bojdak@gmail.com>
2022-10-18 23:33:08 +0200

    tests updated

--- client/src/SessionView/Overview/Session/PublicObjectives/index.test.js
+++ client/src/SessionView/Overview/Session/PublicObjectives/index.test.js
@@ -5,25 +5,44 @@ import { renderWithProviders, getTestQueryClient } from '../../../../testUtils'
 
 import PublicObjectives from './index'
 
+const sessionId = '6fd5c725-30cd-4320-8889-c2f6427ba365'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({ sessionId }),
+}))
+
+let mockSessionObject = null
+
+jest.mock('../../../queries', () => ({
+  useSession: () => ({ session: mockSessionObject }),
+}))
+
 test('should add one point to faction when stage one objective is scored', async () => {
   // given
   const dispatch = () => null
   const updateFactionPoints = jest.fn()
-  const session = {
+  const factions = [
+    'The_Embers_of_Muaat',
+    'The_Naalu_Collective',
+    'The_Universities_of_Jol__Nar',
+    'The_Nomad',
+  ]
+
+  mockSessionObject = {
     objectives: [{ slug: 'diversify-research', scoredBy: [] }],
-    factions: [
-      'The_Embers_of_Muaat',
-      'The_Naalu_Collective',
-      'The_Universities_of_Jol__Nar',
-      'The_Nomad',
-    ],
+    factions,
+    players: factions.map((faction, idx) => ({
+      playerName: `Player ${idx}`,
+      faction,
+    })),
     points: [
       { faction: 'The_Embers_of_Muaat', points: 0 },
       { faction: 'The_Naalu_Collective', points: 0 },
       { faction: 'The_Universities_of_Jol__Nar', points: 0 },
       { faction: 'The_Nomad', points: 0 },
     ],
-    id: '6fd5c725-30cd-4320-8889-c2f6427ba365',
+    id: sessionId,
     events: null,
     createdAt: '0001-01-01T00:00:00+00:00',
     remote: true,
@@ -34,19 +53,19 @@ test('should add one point to faction when stage one objective is scored', async
     <ComboDispatchContext.Provider value={dispatch}>
       <PublicObjectives
         editable
-        session={session}
+        session={mockSessionObject}
         updateFactionPoints={updateFactionPoints}
       />
     </ComboDispatchContext.Provider>,
   )
 
   // when
-  fireEvent.click(await findByTitle('The Universities of Jol-Nar'))
+  fireEvent.click(await findByTitle('The Universities of Jol-Nar (Player 2)'))
 
   // then
   expect(updateFactionPoints).toHaveBeenCalledWith({
     points: 1,
-    sessionId: session.id,
+    sessionId,
     faction: 'The_Universities_of_Jol__Nar',
   })
 })
@@ -55,21 +74,27 @@ test('should add two points to faction when second stage objective is scored', a
   // given
   const dispatch = () => null
   const updateFactionPoints = jest.fn()
-  const session = {
+  const factions = [
+    'The_Embers_of_Muaat',
+    'The_Naalu_Collective',
+    'The_Universities_of_Jol__Nar',
+    'The_Nomad',
+  ]
+
+  mockSessionObject = {
     objectives: [{ slug: 'achieve-supremacy', scoredBy: [] }],
-    factions: [
-      'The_Embers_of_Muaat',
-      'The_Naalu_Collective',
-      'The_Universities_of_Jol__Nar',
-      'The_Nomad',
-    ],
+    factions,
+    players: factions.map((faction, idx) => ({
+      playerName: `Player ${idx}`,
+      faction,
+    })),
     points: [
       { faction: 'The_Embers_of_Muaat', points: 0 },
       { faction: 'The_Naalu_Collective', points: 0 },
       { faction: 'The_Universities_of_Jol__Nar', points: 0 },
       { faction: 'The_Nomad', points: 0 },
     ],
-    id: '6fd5c725-30cd-4320-8889-c2f6427ba365',
+    id: sessionId,
     events: null,
     createdAt: '0001-01-01T00:00:00+00:00',
     remote: true,
@@ -80,19 +105,19 @@ test('should add two points to faction when second stage objective is scored', a
     <ComboDispatchContext.Provider value={dispatch}>
       <PublicObjectives
         editable
-        session={session}
+        session={mockSessionObject}
         updateFactionPoints={updateFactionPoints}
       />
     </ComboDispatchContext.Provider>,
   )
 
   // when
-  fireEvent.click(await findByTitle('The Universities of Jol-Nar'))
+  fireEvent.click(await findByTitle('The Universities of Jol-Nar (Player 2)'))
 
   // then
   expect(updateFactionPoints).toHaveBeenCalledWith({
     points: 2,
-    sessionId: session.id,
+    sessionId,
     faction: 'The_Universities_of_Jol__Nar',
   })
 })
