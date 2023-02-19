f92ee00cf97e43105f493319d3731eee77679a6f
tarnas14 <tarnas14@gmail.com>
2023-02-19 16:04:15 +0100

    fix session summary condition for winner

--- serverTests/SessionSummary.cs
+++ serverTests/SessionSummary.cs
@@ -286,6 +286,168 @@ namespace ServerTests
         }
 
         [Test]
+        public void ShouldIncludeSessionSummaryIfPlayerScoresPointsToExceedVictoryPointTarget()
+        {
+            // given
+            var given = new List<GameEvent>()
+            {
+                new GameEvent
+                {
+                    EventType = nameof(CommitDraft),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new CommitDraftPayload
+                    {
+                        Factions = new string[] { "The_Nekro_Virus", "Sardakk_Norr", "The_Winnu", "The_Xxcha_Kingdom" },
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(ObjectiveAdded),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new ObjectiveAddedPayload
+                    {
+                            Slug = "raise-a-fleet",
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(VictoryPointsUpdated),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new VictoryPointsUpdatedPayload
+                    {
+                            Faction = "The_Xxcha_Kingdom",
+                            Points = 1,
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(VictoryPointsUpdated),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new VictoryPointsUpdatedPayload
+                    {
+                            Faction = "The_Nekro_Virus",
+                            Points = 4,
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(VictoryPointsUpdated),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new VictoryPointsUpdatedPayload
+                    {
+                            Faction = "Sardakk_Norr",
+                            Points = 2,
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(VictoryPointsUpdated),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new VictoryPointsUpdatedPayload
+                    {
+                            Faction = "The_Winnu",
+                            Points = 9,
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(VictoryPointsUpdated),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new VictoryPointsUpdatedPayload
+                    {
+                            Faction = "Sardakk_Norr",
+                            Points = 3,
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(ObjectiveScored),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new ObjectiveScoredPayload
+                    {
+                            Faction = "The_Winnu",
+                            Slug = "centralize-galactic-trade",
+                            Points = 11,
+                    }, this.SerializerSettings),
+                },
+                new GameEvent
+                {
+                    EventType = nameof(VictoryPointsUpdated),
+                    SerializedPayload = JsonConvert.SerializeObject(
+                        new VictoryPointsUpdatedPayload
+                    {
+                            Faction = "The_Winnu",
+                            Points = 11,
+                    }, this.SerializerSettings),
+                },
+            };
+            var expected = new List<TimelineEvent>()
+            {
+                new TimelineEvent
+                {
+                    Order = 0,
+                    EventType = "CommitDraft",
+                    SerializedPayload = "{\"factions\":[\"The_Nekro_Virus\",\"Sardakk_Norr\",\"The_Winnu\",\"The_Xxcha_Kingdom\"]}",
+                },
+                new TimelineEvent
+                {
+                    Order = 1,
+                    EventType = "ObjectiveAdded",
+                    SerializedPayload = "{\"slug\":\"raise-a-fleet\"}",
+                },
+                new TimelineEvent
+                {
+                    Order = 2,
+                    EventType = "VictoryPointsUpdated",
+                    SerializedPayload = "{\"faction\":\"The_Xxcha_Kingdom\",\"points\":1,\"source\":0,\"context\":null}",
+                },
+                new TimelineEvent
+                {
+                    Order = 3,
+                    EventType = "VictoryPointsUpdated",
+                    SerializedPayload = "{\"faction\":\"The_Nekro_Virus\",\"points\":4,\"source\":0,\"context\":null}",
+                },
+                new TimelineEvent
+                {
+                    Order = 4,
+                    EventType = "VictoryPointsUpdated",
+                    SerializedPayload = "{\"faction\":\"Sardakk_Norr\",\"points\":2,\"source\":0,\"context\":null}",
+                },
+                new TimelineEvent
+                {
+                    Order = 5,
+                    EventType = "VictoryPointsUpdated",
+                    SerializedPayload = "{\"faction\":\"The_Winnu\",\"points\":9,\"source\":0,\"context\":null}",
+                },
+                new TimelineEvent
+                {
+                    Order = 6,
+                    EventType = "VictoryPointsUpdated",
+                    SerializedPayload = "{\"faction\":\"Sardakk_Norr\",\"points\":3,\"source\":0,\"context\":null}",
+                },
+                new TimelineEvent
+                {
+                    Order = 7,
+                    EventType = "ObjectiveScored",
+                    SerializedPayload = "{\"slug\":\"centralize-galactic-trade\",\"faction\":\"The_Winnu\",\"points\":11}",
+                },
+                new TimelineEvent
+                {
+                    Order = 8,
+                    EventType = "SessionSummary",
+                    SerializedPayload = "{\"winner\":\"The_Winnu\",\"results\":[{\"faction\":\"The_Xxcha_Kingdom\",\"points\":1},{\"faction\":\"The_Nekro_Virus\",\"points\":4},{\"faction\":\"Sardakk_Norr\",\"points\":3},{\"faction\":\"The_Winnu\",\"points\":11}]}",
+                },
+            };
+            var timeline = new Timeline(new Session { Events = given });
+
+            // when
+            var actual = timeline.AddSessionSummary().GetEvents();
+
+            // then
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
         public void ShouldIncludeSessionSummaryForUpdatedVpCount()
         {
             // given
