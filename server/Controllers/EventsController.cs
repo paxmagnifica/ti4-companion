414cb21188eefc2895577076bbd8ca05152d96ed
tarnas14 <tarnas14@gmail.com>
2023-09-24 12:03:01 +0200

    added release notes for data integrity updates
    issue: #326

--- server/Controllers/EventsController.cs
+++ server/Controllers/EventsController.cs
@@ -33,8 +33,9 @@ namespace Server.Controllers
 
                 if (!eventAllowed)
                 {
-                    return new BadRequestObjectResult(new {
-                        tiCompanionError = "stale_state_event"
+                    return new BadRequestObjectResult(new
+                    {
+                        tiCompanionError = "stale_state_event",
                     });
                 }
 
