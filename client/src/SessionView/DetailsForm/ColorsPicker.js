ec5e093199c5c44f8d17d13dfc3365c52e8d4317
Mateusz Tarnaski <tarnas14@gmail.com>
2023-10-30 12:30:56 +0100

    fixed color assignment on mobile view (#341)
    * fixed color assignment on mobile view
    
    issue: #325
    
    * release notes
    
    issue: #325

--- client/src/SessionView/DetailsForm/ColorsPicker.js
+++ client/src/SessionView/DetailsForm/ColorsPicker.js
@@ -2,6 +2,7 @@ import { Typography, Grid, TextField } from '@material-ui/core'
 
 import { useTranslation, Trans } from '../../i18n'
 import { ColorPicker } from '../../shared/ColorPicker'
+import PlayerFlag from '../PlayerFlag'
 
 export function ColorsPicker({ colors, players, onChange, disabled }) {
   const { t } = useTranslation()
@@ -13,39 +14,51 @@ export function ColorsPicker({ colors, players, onChange, disabled }) {
           <Trans i18nKey="sessionDetails.colorsPicker.sectionTitle" />
         </Typography>
       </Grid>
-      <Grid item style={{ display: 'flex', gridColumnGap: '1em' }} xs={12}>
-        {players.map(({ faction: factionKey, playerName }) => (
-          <div key={factionKey} style={{ display: 'flex' }}>
-            <TextField
-              color="secondary"
-              disabled
-              label={playerName}
-              value={t(`factions.${factionKey}.name`)}
-              variant="filled"
-            />
-            <ColorPicker
-              color={colors[factionKey] || null}
-              disabled={disabled}
-              onChange={(value) => {
-                onChange((oldColors) => {
-                  const factionWithThisColor = Object.entries(oldColors).find(
-                    ([, colorName]) => colorName === value,
-                  )
+      {players.map(({ faction: factionKey, playerName }) => (
+        <Grid
+          key={factionKey}
+          item
+          md={3}
+          sm={6}
+          style={{ display: 'flex' }}
+          xs={12}
+        >
+          <PlayerFlag
+            disabled
+            factionKey={factionKey}
+            height="56px"
+            selected
+            width="66px"
+          />
+          <TextField
+            color="secondary"
+            disabled
+            label={playerName}
+            value={t(`factions.${factionKey}.name`)}
+            variant="filled"
+          />
+          <ColorPicker
+            color={colors[factionKey] || null}
+            disabled={disabled}
+            onChange={(value) => {
+              onChange((oldColors) => {
+                const factionWithThisColor = Object.entries(oldColors).find(
+                  ([, colorName]) => colorName === value,
+                )
 
-                  return {
-                    ...oldColors,
-                    ...(factionWithThisColor
-                      ? { [factionWithThisColor[0]]: '' }
-                      : {}),
-                    [factionKey]: value,
-                  }
-                })
-              }}
-              style={{ marginLeft: '0.3em' }}
-            />
-          </div>
-        ))}
-      </Grid>
+                return {
+                  ...oldColors,
+                  ...(factionWithThisColor
+                    ? { [factionWithThisColor[0]]: '' }
+                    : {}),
+                  [factionKey]: value,
+                }
+              })
+            }}
+            style={{ marginLeft: '0.3em' }}
+          />
+        </Grid>
+      ))}
     </>
   )
 }
