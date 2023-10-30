95867719625f5fcb330140f3d83ce49b04d5cc9e
tarnas14 <tarnas14@gmail.com>
2023-10-30 14:06:44 +0100

    update frontend tests for public objective scoring

--- client/src/SessionView/Overview/Session/PublicObjectives/index.test.js
+++ client/src/SessionView/Overview/Session/PublicObjectives/index.test.js
@@ -15,10 +15,9 @@ jest.mock('../../../useSessionContext', () => ({
   }),
 }))
 
-test('should add one point to faction when stage one objective is scored', async () => {
+test('should add first point to faction when stage one objective is scored', async () => {
   // given
-  const dispatch = () => null
-  const updateFactionPoints = jest.fn()
+  const dispatch = jest.fn()
   const factions = [
     'The_Embers_of_Muaat',
     'The_Naalu_Collective',
@@ -48,11 +47,7 @@ test('should add one point to faction when stage one objective is scored', async
   const { findByTitle } = renderWithProviders(
     getTestQueryClient(),
     <ComboDispatchContext.Provider value={dispatch}>
-      <PublicObjectives
-        editable
-        session={mockSessionObject}
-        updateFactionPoints={updateFactionPoints}
-      />
+      <PublicObjectives editable session={mockSessionObject} />
     </ComboDispatchContext.Provider>,
   )
 
@@ -60,17 +55,71 @@ test('should add one point to faction when stage one objective is scored', async
   fireEvent.click(await findByTitle('The Universities of Jol-Nar (Player 2)'))
 
   // then
-  expect(updateFactionPoints).toHaveBeenCalledWith({
-    points: 1,
-    sessionId,
-    faction: 'The_Universities_of_Jol__Nar',
+  expect(dispatch).toHaveBeenCalledWith({
+    type: 'ObjectiveScored',
+    payload: {
+      sessionId,
+      points: 1,
+      faction: 'The_Universities_of_Jol__Nar',
+      slug: 'diversify-research',
+    },
   })
 })
 
-test('should add two points to faction when second stage objective is scored', async () => {
+test('should add one point to faction points when stage one objective is scored', async () => {
   // given
-  const dispatch = () => null
-  const updateFactionPoints = jest.fn()
+  const dispatch = jest.fn()
+  const factions = [
+    'The_Embers_of_Muaat',
+    'The_Naalu_Collective',
+    'The_Universities_of_Jol__Nar',
+    'The_Nomad',
+  ]
+
+  mockSessionObject = {
+    objectives: [{ slug: 'diversify-research', scoredBy: [] }],
+    factions,
+    players: factions.map((faction, idx) => ({
+      playerName: `Player ${idx}`,
+      faction,
+    })),
+    points: [
+      { faction: 'The_Embers_of_Muaat', points: 0 },
+      { faction: 'The_Naalu_Collective', points: 0 },
+      { faction: 'The_Universities_of_Jol__Nar', points: 2 },
+      { faction: 'The_Nomad', points: 0 },
+    ],
+    id: sessionId,
+    events: null,
+    createdAt: '0001-01-01T00:00:00+00:00',
+    remote: true,
+  }
+
+  const { findByTitle } = renderWithProviders(
+    getTestQueryClient(),
+    <ComboDispatchContext.Provider value={dispatch}>
+      <PublicObjectives editable session={mockSessionObject} />
+    </ComboDispatchContext.Provider>,
+  )
+
+  // when
+  fireEvent.click(await findByTitle('The Universities of Jol-Nar (Player 2)'))
+
+  // then
+  expect(dispatch).toHaveBeenCalledWith({
+    type: 'ObjectiveScored',
+    payload: {
+      sessionId,
+      points: 3,
+      faction: 'The_Universities_of_Jol__Nar',
+      slug: 'diversify-research',
+    },
+  })
+})
+
+test('should add two points to faction with 0 points when second stage objective is scored', async () => {
+  // given
+  const dispatch = jest.fn()
   const factions = [
     'The_Embers_of_Muaat',
     'The_Naalu_Collective',
@@ -100,11 +149,7 @@ test('should add two points to faction when second stage objective is scored', a
   const { findByTitle } = renderWithProviders(
     getTestQueryClient(),
     <ComboDispatchContext.Provider value={dispatch}>
-      <PublicObjectives
-        editable
-        session={mockSessionObject}
-        updateFactionPoints={updateFactionPoints}
-      />
+      <PublicObjectives editable session={mockSessionObject} />
     </ComboDispatchContext.Provider>,
   )
 
@@ -112,9 +157,64 @@ test('should add two points to faction when second stage objective is scored', a
   fireEvent.click(await findByTitle('The Universities of Jol-Nar (Player 2)'))
 
   // then
-  expect(updateFactionPoints).toHaveBeenCalledWith({
-    points: 2,
-    sessionId,
-    faction: 'The_Universities_of_Jol__Nar',
+  expect(dispatch).toHaveBeenCalledWith({
+    type: 'ObjectiveScored',
+    payload: {
+      points: 2,
+      sessionId,
+      faction: 'The_Universities_of_Jol__Nar',
+      slug: 'achieve-supremacy',
+    },
+  })
+})
+
+test('should add two points to faction with points when second stage objective is scored', async () => {
+  // given
+  const dispatch = jest.fn()
+  const factions = [
+    'The_Embers_of_Muaat',
+    'The_Naalu_Collective',
+    'The_Universities_of_Jol__Nar',
+    'The_Nomad',
+  ]
+
+  mockSessionObject = {
+    objectives: [{ slug: 'achieve-supremacy', scoredBy: [] }],
+    factions,
+    players: factions.map((faction, idx) => ({
+      playerName: `Player ${idx}`,
+      faction,
+    })),
+    points: [
+      { faction: 'The_Embers_of_Muaat', points: 0 },
+      { faction: 'The_Naalu_Collective', points: 0 },
+      { faction: 'The_Universities_of_Jol__Nar', points: 4 },
+      { faction: 'The_Nomad', points: 0 },
+    ],
+    id: sessionId,
+    events: null,
+    createdAt: '0001-01-01T00:00:00+00:00',
+    remote: true,
+  }
+
+  const { findByTitle } = renderWithProviders(
+    getTestQueryClient(),
+    <ComboDispatchContext.Provider value={dispatch}>
+      <PublicObjectives editable session={mockSessionObject} />
+    </ComboDispatchContext.Provider>,
+  )
+
+  // when
+  fireEvent.click(await findByTitle('The Universities of Jol-Nar (Player 2)'))
+
+  // then
+  expect(dispatch).toHaveBeenCalledWith({
+    type: 'ObjectiveScored',
+    payload: {
+      points: 6,
+      sessionId,
+      faction: 'The_Universities_of_Jol__Nar',
+      slug: 'achieve-supremacy',
+    },
   })
 })
